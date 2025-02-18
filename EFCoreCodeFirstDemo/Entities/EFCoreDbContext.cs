caafc4e464417e7799fcf725f2cd27ca67ac1e64
jfsom <vijay.jfsom@gmail.com>
2025-02-18 23:12:44 +0530

    Create Operation using Entity Framework Core

--- EFCoreCodeFirstDemo/Entities/EFCoreDbContext.cs
+++ EFCoreCodeFirstDemo/Entities/EFCoreDbContext.cs
@@ -1,35 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace EFCoreCodeFirstDemo.Entities
 {
     // EFCoreDbContext is your custom DbContext class that extends the base DbContext class provided by EF Core.
     public class EFCoreDbContext : DbContext
     {
-        // The OnConfiguring method allows us to configure the DbContext options,
-        // such as specifying the database provider and connection string.
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            // Step 1: Load the Configuration File (appsettings.json).
-            // The ConfigurationBuilder class is used to construct configuration settings from various sources.
-            // Here, we add the appsettings.json file to the configuration sources and then build it.
-            var configBuilder = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json") // Specify the configuration file to load.
-                .Build(); // Build the configuration object, making it ready to retrieve values.
+            // Display the generated SQL queries in the Console window
+            optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
 
-            // Step 2: Get the "ConnectionStrings" section from the configuration.
-            // The GetSection method is used to access a specific section within the configuration file.
-            // Here, we are accessing the "ConnectionStrings" section which contains our database connection strings.
-            var configSection = configBuilder.GetSection("ConnectionStrings");
-
-            // Step 3: Retrieve the connection string value using its key ("SQLServerConnection").
-            // The indexer [] is used to access the value corresponding to the "SQLServerConnection" key within the section.
-            // The null-coalescing operator (??) ensures that if the key is not found, it will return null.
-            var connectionString = configSection["SQLServerConnection"] ?? null;
-
-            // Step 4: Configure the DbContext to use SQL Server with the retrieved connection string.
-            // The UseSqlServer method is an extension method that configures the context to connect to a SQL Server database.
-            optionsBuilder.UseSqlServer(connectionString);
+            // Configure the connection string
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-RUC57UF;Database=EFCoreDB1;Trusted_Connection=True;TrustServerCertificate=True;");
         }
 
         // DbSet<Student> corresponds to the Students table in the database.
--- EFCoreCodeFirstDemo/Program.cs
+++ EFCoreCodeFirstDemo/Program.cs
@@ -1,5 +1,4 @@
 ﻿using EFCoreCodeFirstDemo.Entities;
-using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreCodeFirstDemo
 {
@@ -9,27 +8,80 @@ namespace EFCoreCodeFirstDemo
         {
             try
             {
-                // Create an instance of the DbContext class
-                using var context = new EFCoreDbContext();
+                // Create an instance of your EFCoreDbContext to interact with the database
+                using (var context = new EFCoreDbContext())
+                {
+                    // Create two new Branch objects
+                    var CSEBranch = new Branch
+                    {
+                        BranchName = "Computer Science",
+                        Description = "Focuses on software development and computing technologies.",
+                        PhoneNumber = "123-456-7890",
+                        Email = "cs@dotnettutorials.net"
+                    };
 
-                GetAllStudents(context);
+                    var ElectricalBranch = new Branch
+                    {
+                        BranchName = "Electrical Engineering",
+                        Description = "Focuses on electrical systems and circuit design.",
+                        PhoneNumber = "987-654-3210",
+                        Email = "ee@dotnettutorials.net"
+                    };
+
+                    // Adding the Branch objects to the Branches DbSet
+                    // This prepares the object to be inserted into the database
+                    context.Branches.Add(CSEBranch);
+                    context.Branches.Add(ElectricalBranch);
+
+                    // Alternatively, you can use the DbContext.Add method to add entities
+                    // context.Add(CSEBranch);
+                    // context.Add(ElectricalBranch);
+
+                    // Create two new Student objects
+                    var student1 = new Student
+                    {
+                        FirstName = "Pranaya",
+                        LastName = "Rout",
+                        DateOfBirth = new DateTime(2000, 1, 15),
+                        Gender = "Female",
+                        Email = "Pranaya.Rout@dotnettutorials.net",
+                        PhoneNumber = "555-1234",
+                        EnrollmentDate = DateTime.Now,
+                        Branch = CSEBranch // Assign the Computer Science branch
+                    };
+
+                    var student2 = new Student
+                    {
+                        FirstName = "Rakesh",
+                        LastName = "Kumar",
+                        DateOfBirth = new DateTime(1999, 10, 22),
+                        Gender = "Male",
+                        Email = "Rakesh.Kumar@dotnettutorials.net",
+                        PhoneNumber = "555-5678",
+                        EnrollmentDate = DateTime.Now,
+                        Branch = ElectricalBranch // Assign the Electrical Engineering branch
+                    };
+
+                    // Adding the Student objects to the Students DbSet
+                    // This prepares the object to be inserted into the database
+                    context.Students.Add(student1);
+                    context.Students.Add(student2);
+
+                    // Alternatively, you can use the DbContext.Add method to add entities
+                    // context.Add(student1);
+                    // context.Add(student2);
+
+                    // Save the changes to the database
+                    // This actually performs the INSERT operation in the database
+                    context.SaveChanges();
+
+                    // Display a success message on the console
+                    Console.WriteLine("Branch and Student records have been successfully inserted into the database.");
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error: {ex.Message}"); ;
-            }
-        }
-
-        private static void GetAllStudents(EFCoreDbContext context)
-        {
-            // Retrieve all students from the context
-            var students = context.Students.Include(s => s.Branch).ToList();
-
-            // Display the students in the console
-            Console.WriteLine("All Students:");
-            foreach (var student in students)
-            {
-                Console.WriteLine($"\t{student.StudentId}: {student.FirstName} {student.LastName}, Branch: {student.Branch?.BranchName}");
             }
         }
     }
