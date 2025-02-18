4e88828ec3fce621b5b6e0ea00774f4b81643687
jfsom <vijay.jfsom@gmail.com>
2025-02-18 23:17:34 +0530

    How Do We Perform Update Operations using EF Core

--- EFCoreCodeFirstDemo/Program.cs
+++ EFCoreCodeFirstDemo/Program.cs
@@ -11,72 +11,39 @@ namespace EFCoreCodeFirstDemo
                 // Create an instance of your EFCoreDbContext to interact with the database
                 using (var context = new EFCoreDbContext())
                 {
-                    // Create two new Branch objects
-                    var CSEBranch = new Branch
+                    // Retrieve the student whose Id is 1 from the database using Find method
+                    // The Find method takes the Primary Key
+                    var student = context.Students.Find(1);
+
+                    // Check if a student was found with ID 1
+                    if (student != null)
                     {
-                        BranchName = "Computer Science",
-                        Description = "Focuses on software development and computing technologies.",
-                        PhoneNumber = "123-456-7890",
-                        Email = "cs@dotnettutorials.net"
-                    };
+                        // Display the original data before updating
+                        Console.WriteLine("Original Student Data:");
+                        Console.WriteLine($"Name: {student.FirstName} {student.LastName}");
+                        Console.WriteLine($"Email: {student.Email}");
 
-                    var ElectricalBranch = new Branch
+                        // Modify the student's properties that need to be updated
+                        student.FirstName = "Paresh"; // Changing the first name
+                        student.LastName = "Mohanty"; // Changing the last name
+                        student.Email = "Paresh.Mohanty@dotnettutrials.net"; // Changing the email
+
+                        // Save the changes to the database
+                        context.SaveChanges();
+
+                        // Display a success message on the console
+                        Console.WriteLine("Student data has been successfully updated.");
+
+                        // Display the updated data
+                        Console.WriteLine("Updated Student Data:");
+                        Console.WriteLine($"Name: {student.FirstName} {student.LastName}");
+                        Console.WriteLine($"Email: {student.Email}");
+                    }
+                    else
                     {
-                        BranchName = "Electrical Engineering",
-                        Description = "Focuses on electrical systems and circuit design.",
-                        PhoneNumber = "987-654-3210",
-                        Email = "ee@dotnettutorials.net"
-                    };
-
-                    // Adding the Branch objects to the Branches DbSet
-                    // This prepares the object to be inserted into the database
-                    context.Branches.Add(CSEBranch);
-                    context.Branches.Add(ElectricalBranch);
-
-                    // Alternatively, you can use the DbContext.Add method to add entities
-                    // context.Add(CSEBranch);
-                    // context.Add(ElectricalBranch);
-
-                    // Create two new Student objects
-                    var student1 = new Student
-                    {
-                        FirstName = "Pranaya",
-                        LastName = "Rout",
-                        DateOfBirth = new DateTime(2000, 1, 15),
-                        Gender = "Female",
-                        Email = "Pranaya.Rout@dotnettutorials.net",
-                        PhoneNumber = "555-1234",
-                        EnrollmentDate = DateTime.Now,
-                        Branch = CSEBranch // Assign the Computer Science branch
-                    };
-
-                    var student2 = new Student
-                    {
-                        FirstName = "Rakesh",
-                        LastName = "Kumar",
-                        DateOfBirth = new DateTime(1999, 10, 22),
-                        Gender = "Male",
-                        Email = "Rakesh.Kumar@dotnettutorials.net",
-                        PhoneNumber = "555-5678",
-                        EnrollmentDate = DateTime.Now,
-                        Branch = ElectricalBranch // Assign the Electrical Engineering branch
-                    };
-
-                    // Adding the Student objects to the Students DbSet
-                    // This prepares the object to be inserted into the database
-                    context.Students.Add(student1);
-                    context.Students.Add(student2);
-
-                    // Alternatively, you can use the DbContext.Add method to add entities
-                    // context.Add(student1);
-                    // context.Add(student2);
-
-                    // Save the changes to the database
-                    // This actually performs the INSERT operation in the database
-                    context.SaveChanges();
-
-                    // Display a success message on the console
-                    Console.WriteLine("Branch and Student records have been successfully inserted into the database.");
+                        // Display a message if no student was found
+                        Console.WriteLine("No student found to update.");
+                    }
                 }
             }
             catch (Exception ex)
