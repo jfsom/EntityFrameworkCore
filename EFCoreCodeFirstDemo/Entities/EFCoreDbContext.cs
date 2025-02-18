97a4a2e9879e7d1ad5a6d6f59e786d29e04502b7
jfsom <vijay.jfsom@gmail.com>
2025-02-18 22:06:55 +0530

    Basic EF Functionality

--- EFCoreCodeFirstDemo/Entities/EFCoreDbContext.cs
+++ EFCoreCodeFirstDemo/Entities/EFCoreDbContext.cs
@@ -1,37 +1,25 @@
-﻿// Import the Entity Framework Core namespace to access DbContext and other EF Core functionalities.
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreCodeFirstDemo.Entities
 {
-    // EFCoreDbContext class inherits from DbContext, which is the primary class for interacting with the database using EF Core.
+    // EFCoreDbContext is your custom DbContext class that extends the base DbContext class provided by EF Core.
     public class EFCoreDbContext : DbContext
     {
-        // Constructor that accepts DbContextOptions<EFCoreDbContext> as a parameter.
-        // The options parameter contains the settings required by EF Core to configure the DbContext,
-        // such as the connection string and provider.
-        public EFCoreDbContext(DbContextOptions<EFCoreDbContext> options)
-        : base(options) // The base(options) call passes the options to the base DbContext class constructor.
-        {
-        }
-
-        // OnConfiguring is an override method that allows configuring the DbContext options,
-        // like setting the database provider and connection string.
+        // The OnConfiguring method allows us to configure the DbContext options,
+        // such as specifying the database provider and connection string.
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            // Configure the database provider and connection string.
-            // UseSqlServer method configures the DbContext to use SQL Server as the database provider.
-            // The provided connection string specifies the server, database name, and credentials.
-            // Replace "Server=YourServerName;Database=YourDatabaseName;User Id=YourUsername;Password=YourPassword;"
-            // with your actual SQL Server details.
-            optionsBuilder.UseSqlServer("Server=DESKTOP-RUC57UF;Database=YourDatabaseName;Integrated Security=False");
+            // Configuring the connection string to use a SQL Server database.
+            // UseSqlServer is an extension method that configures the context to connect to a SQL Server database.
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-RUC57UF;Database=EFCoreDB1;Trusted_Connection=True;TrustServerCertificate=True;");
         }
 
-        // DbSet<Student> Students represents a table in the database corresponding to the Student entity.
-        // EF Core uses DbSet<TEntity> to track changes and execute queries related to the Student entity.
+        // DbSet<Student> corresponds to the Students table in the database.
+        // It allows EF Core to track and manage Student entities.
         public DbSet<Student> Students { get; set; }
 
-        // DbSet<Branch> Branches represents a table in the database corresponding to the Branch entity.
-        // Similar to the Students DbSet, this property is used by EF Core to track and manage Branch entities.
+        // DbSet<Branch> corresponds to the Branches table in the database.
+        // It allows EF Core to track and manage Branch entities.
         public DbSet<Branch> Branches { get; set; }
     }
 }
