0a9365660bc7d2efffbb0c9061511be31f3c44f5
jfsom <vijay.jfsom@gmail.com>
2025-02-20 14:01:51 +0530

    using Table Attribute [Table("StudentInfo")]

--- EFCoreCodeFirstDemo/Entities/EFCoreDbContext.cs
+++ EFCoreCodeFirstDemo/Entities/EFCoreDbContext.cs
@@ -12,8 +12,5 @@ namespace EFCoreCodeFirstDemo.Entities
         // DbSet properties represent the tables in the database. 
         // Each DbSet corresponds to a table, and the type parameter corresponds to the entity class mapped to that table.
         public DbSet<Student> Students { get; set; }
-        public DbSet<Teacher> Teachers { get; set; }
-        public DbSet<Course> Courses { get; set; }
-        public DbSet<Address> Addresses { get; set; }
     }
 }
--- EFCoreCodeFirstDemo/Entities/Student.cs
+++ EFCoreCodeFirstDemo/Entities/Student.cs
@@ -1,18 +1,12 @@
-﻿using System.Reflection;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFCoreCodeFirstDemo.Entities
 {
+    [Table("StudentInfo")]  // Mapping the entity to StudentInfo table in dbo schema
     public class Student
     {
-        public int StudentId { get; set; }  // SQL Type: INT (NOT NULL, Primary Key)
-        public string FirstName { get; set; }  // SQL Type: NVARCHAR(MAX) (NOT NULL)
-        public string? LastName { get; set; }  // SQL Type: NVARCHAR(MAX) (NULL)
-        public DateTime? DateOfBirth { get; set; }  // SQL Type: DATETIME2 (NULL)
-        public decimal GPA { get; set; }  // SQL Type: DECIMAL(18, 2) (NOT NULL)
-        public bool IsActive { get; set; }  // SQL Type: BIT (NOT NULL)
-        public byte[] ProfilePicture { get; set; }  // SQL Type: VARBINARY(MAX) (NOT NULL)
-        public virtual Gender Gender { get; set; }  // SQL Type: INT (NOT NULL, because enums are stored as INT)
-        public virtual Address Address { get; set; }  // SQL Type: This would create a foreign key with default INT (if Address is required, it would be NOT NULL)
-        public virtual ICollection<Course> Courses { get; set; }  // SQL Type: This would create a join table for many-to-many relationships
+        public int StudentId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
     }
 }
