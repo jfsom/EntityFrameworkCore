95e5b259be25b04ac07547d5ad36e8fc7afd0054
jfsom <vijay.jfsom@gmail.com>
2025-02-20 21:23:27 +0530

    ForeignKey Attribute On main table

--- EFCoreCodeFirstDemo/Entities/Employee.cs
+++ EFCoreCodeFirstDemo/Entities/Employee.cs
@@ -1,14 +1,16 @@
-﻿namespace EFCoreCodeFirstDemo.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCoreCodeFirstDemo.Entities
 {
     public class Employee
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
-        //To Create a Foreign Key pointing to the Id column of the Departments table
-        //it should have the Department Navigation Property
-        public Department Department { get; set; }
+        [ForeignKey("Department")]
+        public int DepartmentReferenceId { get; set; }
 
-        //In this case, EF Core automatically create the DepartmentId Foreign Key in the Employees table
+        //Related Standard Navigational Property
+        public Department Department { get; set; }
     }
 }
