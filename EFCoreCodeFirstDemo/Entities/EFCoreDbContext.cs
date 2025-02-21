333cdc4053a51ce591d1f7f252be284a30843745
jfsom <vijay.jfsom@gmail.com>
2025-02-21 17:58:01 +0530

    TableColumnsCreationBasedOnNullableAndNonNullableProperty

--- EFCoreCodeFirstDemo/Entities/EFCoreDbContext.cs
+++ EFCoreCodeFirstDemo/Entities/EFCoreDbContext.cs
@@ -8,19 +8,6 @@ namespace EFCoreCodeFirstDemo.Entities
             optionsBuilder.UseSqlServer(@"Server=DESKTOP-RUC57UF;Database=StudentDB;Trusted_Connection=True;TrustServerCertificate=True;");
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // Seed data for the Expense entity
-            modelBuilder.Entity<Expense>().HasData(
-                new Expense { ExpenseId = 1, Department = "HR", Purpose = "Office Supplies", Amount = 1500, Date = new DateTime(2024, 9, 15) },
-                new Expense { ExpenseId = 2, Department = "IT", Purpose = "Software License", Amount = 3500, Date = new DateTime(2024, 9, 16) },
-                new Expense { ExpenseId = 3, Department = "IT", Purpose = "Team Lunch", Amount = 800, Date = new DateTime(2024, 9, 17) },
-                new Expense { ExpenseId = 4, Department = "HR", Purpose = "Training", Amount = 2500, Date = new DateTime(2024, 9, 18) },
-                new Expense { ExpenseId = 5, Department = "IT", Purpose = "Hardware Upgrade", Amount = 5500, Date = new DateTime(2024, 9, 19) }
-            );
-        }
-
-        public DbSet<Expense> Expenses { get; set; }
-        public DbSet<DepartmentExpenseReport> DepartmentExpenseReports { get; set; }
+        public DbSet<Student> Students { get; set; }
     }
 }
---
+++ EFCoreCodeFirstDemo/Entities/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreCodeFirstDemo.Entities
+{
+    public class Student
+    {
+        public int StudentId { get; set; }  // This will create a NOT NULL column
+        public string? Name { get; set; }   // This will create a NULL column
+        public string? Address { get; set; } // This will create a NULL column
+        public int RollNumber { get; set; }  // This will create a NOT NULL column
+    }
+}
