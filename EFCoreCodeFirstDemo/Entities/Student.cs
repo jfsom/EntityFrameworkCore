20632fab7332f02a5e22c5f92f91b98b81c5caee
jfsom <vijay.jfsom@gmail.com>
2025-02-21 10:14:59 +0530

    How to Specify the Index Sort Order  in EF Core

--- EFCoreCodeFirstDemo/Entities/Student.cs
+++ EFCoreCodeFirstDemo/Entities/Student.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 namespace EFCoreCodeFirstDemo.Entities
 {
-    [Index(nameof(RegistrationNumber), Name = "Index_RegistrationNumber", IsUnique = true)]
+    [Index(nameof(RegistrationNumber), nameof(RollNumber), AllDescending = true, Name = "Index_RegistrationNumber_RollNumber")]
     public class Student
     {
         public int StudentId { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public int RegistrationNumber { get; set; }
+        public int RollNumber { get; set; }
     }
 }
---
+++ EFCoreCodeFirstDemo/Migrations/20250221044411_Index_Attribute_IndexSortOrderinEFCore.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreCodeFirstDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class Index_Attribute_IndexSortOrderinEFCore : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "Index_RegistrationNumber",
+                table: "Students");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RollNumber",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "Index_RegistrationNumber_RollNumber",
+                table: "Students",
+                columns: new[] { "RegistrationNumber", "RollNumber" },
+                descending: new bool[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "Index_RegistrationNumber_RollNumber",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "RollNumber",
+                table: "Students");
+
+            migrationBuilder.CreateIndex(
+                name: "Index_RegistrationNumber",
+                table: "Students",
+                column: "RegistrationNumber",
+                unique: true);
+        }
+    }
+}
