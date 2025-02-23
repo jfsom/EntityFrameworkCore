5ca4e08bf823dbc3f3e914ac45d584fb274610ed
jfsom <vijay.jfsom@gmail.com>
2025-02-23 22:49:17 +0530

    Configure the Relationship Using Fluent API

--- EFCoreCodeFirstDemo/Entities/Employee.cs
+++ EFCoreCodeFirstDemo/Entities/Employee.cs
@@ -1,17 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace EFCoreCodeFirstDemo.Entities
+﻿namespace EFCoreCodeFirstDemo.Entities
 {
     public class Employee
     {
         public int EmployeeId { get; set; }
         public string Name { get; set; }
-
-        [ForeignKey("Manager")]
         public int? ManagerId { get; set; }
         public Employee Manager { get; set; }
-
-        [InverseProperty("Manager")]
         public ICollection<Employee> Subordinates { get; set; } = new List<Employee>();
     }
 }
---
+++ EFCoreCodeFirstDemo/Migrations/20250223171827_Fluent-API-Approach-to-Implement-Self-Referential-Relationship.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreCodeFirstDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class FluentAPIApproachtoImplementSelfReferentialRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Employees_ManagerId",
+                table: "Employees");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Employees_ManagerId",
+                table: "Employees",
+                column: "ManagerId",
+                principalTable: "Employees",
+                principalColumn: "EmployeeId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Employees_ManagerId",
+                table: "Employees");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Employees_ManagerId",
+                table: "Employees",
+                column: "ManagerId",
+                principalTable: "Employees",
+                principalColumn: "EmployeeId");
+        }
+    }
+}
