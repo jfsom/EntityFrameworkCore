c362f10a07ad563cba509705e0dce561518253d5
jfsom <vijay.jfsom@gmail.com>
2025-02-25 17:47:30 +0530

    sqlOptions.MaxBatchSize(100) For Inserting Bulk Data

--- EFCoreCodeFirstDemo/Entities/EFCoreDbContext.cs
+++ EFCoreCodeFirstDemo/Entities/EFCoreDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+
 namespace EFCoreCodeFirstDemo.Entities
 {
     public class EFCoreDbContext : DbContext
@@ -8,8 +9,13 @@ namespace EFCoreCodeFirstDemo.Entities
         {
             // Log the SQL queries to the console
             optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
-            // Configuring the Connection String
-            optionsBuilder.UseSqlServer(@"Server=DESKTOP-RUC57UF;Database=StudentDB;Trusted_Connection=True;TrustServerCertificate=True;");
+
+            string _connectionString = @"Server=DESKTOP-RUC57UF;Database=StudentDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+            optionsBuilder.UseSqlServer(_connectionString, sqlOptions =>
+            {
+                sqlOptions.MaxBatchSize(100); // Set the batch size to 100
+            });
         }
 
         public DbSet<Student> Students { get; set; }
